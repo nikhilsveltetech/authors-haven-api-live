105893bc83a4784bfe6d5dec7e4c7ee4f86050e6
nikhil <nikhil.bhardwaj@sveltetech.com>
2022-11-08 16:34:16 +0530

    feat: add custom user model, setup redoc api docs

---
+++ core_apps/users/managers.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.core.validators import validate_email
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
+
+
+class CustomUserManager(BaseUserManager):
+
+    def email_validator(self,email):
+        try:
+            validate_email(email)
+        except ValidationError as e:
+            raise ValueError('You must provide a valid email address') from e
+
+    def create_user(self,username, first_name, last_name, email, password,**extra_fields):
+        if not username:
+            raise ValueError(_('You must provide a username'))
+
+        if not first_name:
+            raise ValueError(_('You must provide a first name'))
+
+        if not last_name:
+            raise ValueError(_('You must provide a last name'))
+
+        if not email:
+            raise ValueError(_("Base user account: An email address is required."))
+
+        email = self.normalize_email(email)
+        self.email_validator(email)
+        
+        user = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,username, first_name, last_name, email, password,**extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superusers must have is_staff=True"))
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superusers must have is_superuser=True"))
+
+        if not password:
+            raise ValueError(_('You must provide a password'))
+
+        if not email:
+            raise ValueError(_("Base user account: An email address is required."))
+
+        email = self.normalize_email(email)
+        self.email_validator(email)
+
+        user = self.create_user(username,first_name, last_name, email, password, **extra_fields)
+        user.save(using=self._db)
+        return user
